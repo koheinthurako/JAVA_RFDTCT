72734f105404cc0013d913591f5db42254b8aff6
koheinthurako <captainthura99@gmail.com>
2025-10-28 20:55:23 +0700

    LET'sST

--- src/C_Inheritance/b_SavingAcc.java
+++ src/C_Inheritance/b_SavingAcc.java
@@ -5,11 +5,15 @@ public class b_SavingAcc extends a_BankAcc{
 	// Inheritance keyword က extends ဆိုပြီးသုံးရတယ် a_BankAcc ဆိုတဲ့ Parent Class ကို Inheritance(extends) လုပ်ပြီဆိုရင်
 	// အရေးကြီးတဲ့အချက်က Parent Class ရဲ့ Constructor ကို Child မှာလည်း ပြန်ခေါ်ပေးရတယ် မခေါ်ရင် Error တက်တယ် 
 	// Parent Class ထဲမှာ ဘယ်လို Constructor ရှိနေလဲဆိုတာကို Inheritance Concept မှာ ရေးပေးရတယ်
+	// Parent မှာ Default Constructor ပဲရှိရင် Child Class ထဲမှာ ဘာမှရေးစရာမလိုပါ
 	
-	public b_SavingAcc(String name, int acc_no, double amount) {
-		// super ဆိုတာ parent class ကို ကိုယ်စားပြုခြင်းဖြစ်ပြီး သူ့ကို Constructor ရဲ့ ထိပ်ဆုံးလိုင်းမှာ ရေးပေးရတယ် မရေးရင် Error တက်တယ်
+	private boolean book;
+	
+	public b_SavingAcc(String name, int acc_no, double amount, boolean book) {
+		// super ဆိုတာ parent class ကို ကိုယ်စားပြုခြင်းဖြစ်ပြီး သူ့ကို Constructor ရဲ့ ထိပ်ဆုံးလိုင်းမှာ ရေးပေးရတယ် မရေးရင် Error တက်တယ် ဒါက Java မှသတ်မှတ်ထားသည့် Rule
 		super(name, acc_no, amount);
 //		System.out.println(super+""); // ဒီလို ရေးရာတွင် super လို့ output ထုတ်တာသည် super.toString() method ကိုခေါ်ခြင်းဖြစ်သည်
+		this.book = book; 
 	}
 	
 }
---
+++ src/C_Inheritance/c_BankTest.java
@@ -0,0 +1,9 @@
+package C_Inheritance;
+
+public class c_BankTest {
+	
+	public static void main(String [] args) {
+		
+	}
+
+}
