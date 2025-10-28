2f0619172418c4383675f624e26fdbd3aa8aefd1
koheinthurako <captainthura99@gmail.com>
2025-10-28 20:51:56 +0700

    MoreABOINHRPARTOCHILogic

--- src/C_Inheritance/a_BankAcc.java
+++ src/C_Inheritance/a_BankAcc.java
@@ -4,5 +4,21 @@ public class a_BankAcc {
 	
 	// Inheritance အစ (အမွေဆက်ခံခြင်း)
 	// Class တစ်ခုကနေ နောက် Class တစ်ခုကို အမွေဆက်ခံထားတယ်ဆိုရင် Single Inheritance
+	// Parent တစ်ခုကနေ Child တစ်ခု၊ အဲ့ Child တစ်ခုကနေ နောက် Child တစ်ခုကထပ်ပြီး အမွေဆက်ခံထားရင် Multiple Inheritance
+	// Parent တစ်ခုကို Child တွေအများကြီးက အမွေဆက်ခံထားရင် Hierarchical Inheritance
+	
+	private String name;
+	private int acc_no;
+	private double amount;
+	
+	public a_BankAcc(String name, int acc_no, double amount) {
+		super();
+		this.name = name;
+		this.acc_no = acc_no;
+		this.amount = amount;
+	}
+	
+	
+	
 
 }
---
+++ src/C_Inheritance/b_SavingAcc.java
@@ -0,0 +1,15 @@
+package C_Inheritance;
+
+public class b_SavingAcc extends a_BankAcc{
+
+	// Inheritance keyword က extends ဆိုပြီးသုံးရတယ် a_BankAcc ဆိုတဲ့ Parent Class ကို Inheritance(extends) လုပ်ပြီဆိုရင်
+	// အရေးကြီးတဲ့အချက်က Parent Class ရဲ့ Constructor ကို Child မှာလည်း ပြန်ခေါ်ပေးရတယ် မခေါ်ရင် Error တက်တယ် 
+	// Parent Class ထဲမှာ ဘယ်လို Constructor ရှိနေလဲဆိုတာကို Inheritance Concept မှာ ရေးပေးရတယ်
+	
+	public b_SavingAcc(String name, int acc_no, double amount) {
+		// super ဆိုတာ parent class ကို ကိုယ်စားပြုခြင်းဖြစ်ပြီး သူ့ကို Constructor ရဲ့ ထိပ်ဆုံးလိုင်းမှာ ရေးပေးရတယ် မရေးရင် Error တက်တယ်
+		super(name, acc_no, amount);
+//		System.out.println(super+""); // ဒီလို ရေးရာတွင် super လို့ output ထုတ်တာသည် super.toString() method ကိုခေါ်ခြင်းဖြစ်သည်
+	}
+	
+}
